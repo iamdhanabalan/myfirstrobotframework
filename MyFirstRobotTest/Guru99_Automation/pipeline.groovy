94163e54a9e5486e26eff5f92bbb2e183b45130b
iamdhanabalan <p_dhanabalan@yahoo.co.in>
2019-05-06 11:44:52 +0200

    Added pipeline script (groovy) for Jenkins

---
+++ MyFirstRobotTest/Guru99_Automation/pipeline.groovy
@@ -0,0 +1,27 @@
+node {
+   stage('Checkout')
+   {
+        checkout([$class: 'GitSCM', branches: [[name: '*/UnderDev']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '20e87360-72a4-425b-b34c-0b5ebae90534', url: 'https://github.com/iamdhanabalan/myfirstrobotframework']]])
+        echo 'Checked out'
+    }
+   stage('Run Tests')
+   {
+        bat label: '', script: 'robot -d Results -i Smoke MyFirstRobotTest\\Guru99_Automation\\Guru99WebAutomation.robot && robotmetrics --inputpath ./Results/'
+   }
+   stage('Generate Report')
+   {
+       step([
+        $class : 'RobotPublisher',
+        outputPath : 'MyFirstRobotTest/Guru99_Automation/Results',
+        outputFileName : "*.xml",
+        disableArchiveOutput : false,
+        passThreshold : 100,
+        unstableThreshold: 95.0,
+        otherFiles : "*.png",
+        ])
+   }
+   stage('Clean WS')
+   {
+       cleanWs()
+   }
+}
