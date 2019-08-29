f8008d5849070e2dd5b03e1b5124fd777c1a3b1a
iamdhanabalan <dondana.rockzz@gmail.com>
2019-08-29 15:11:01 +0200

    Test

--- MyFirstRobotTest/Guru99_Automation/pipeline.groovy
+++ MyFirstRobotTest/Guru99_Automation/pipeline.groovy
@@ -10,6 +10,7 @@ node {
       bat label: '', script: 'robot -v myconfig.yaml -d Results MyFirstRobotTest\\Guru99_Automation\\Guru99WebAutomation.robot && robotmetrics --inputpath ./Results/'
       */
       currentBuild.result = 'UNSTABLE'
+      echo 'Demo worked'
    }
    stage('Generate Report')
    {
